92a14eb19a3155bc7298fb9e843a4385501bccac
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-09-22 12:16:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
-resource "vault_aws_secret_backend" "aws" {
-  access_key = "AKIA....."
-  secret_key = "SECRETKEYFROMAWS"
+resource "kubernetes_secret" "example" {
+  metadata {
+    name = "basic-auth"
+  }
+
+  data = {
+    username = "admin"
+    password = "P4ssw0rd"
+  }
+
+  type = "kubernetes.io/basic-auth"
+}
+
+resource "aws_iam_access_key" "lb" {
+  user    = "IAM"
+  pgp_key = "dbjsbdjb23732hjd"
 }
